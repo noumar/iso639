a98f985f21c5ef16c8f1bd26173a64d0b3975584
Mikael Karlsson <i8myshoes@gmail.com>
2014-07-11 18:48:53 +0300

    Finalise class

--- country.py
+++ country.py
@@ -30,6 +30,50 @@ def map_language(language):
             try: return languages.get(common_name=l.title())
             except KeyError: pass
 
+
+class iso_639_3(object):
+    """
+    This class is a close to drop-in replacement for pycountry.languages.
+    But unlike pycountry.languages it also supports ISO 639-3.
+    """
+    def __init__(self):
+        self.languages = [self.language(a, b, c, d, e) for a, b, c, d, _, _, e, _ in self.fabtabular()]
+        self.alpha3 = {x.alpha3: x for x in self.languages if x.alpha3}
+        self.bibliographic = {x.bibliographic: x for x in self.languages if x.bibliographic}
+        self.terminology = {x.terminology: x for x in self.languages if x.terminology}
+        self.alpha2 = {x.alpha2: x for x in self.languages if x.alpha2}
+        self.name = {x.name: x for x in self.languages if x.name}
+
+    class language(object):
+        def __init__(self, dash3, dash2b, dash2t, dash1, name):
+            self.alpha3 = dash3
+            self.bibliographic = dash2b
+            self.terminology = dash2t
+            self.alpha2 = dash1
+            self.name = name
+
+    def get(self, **kwargs):
+        if not len(kwargs) == 1:
+            raise AttributeError("Only one keyword expected")
+        key, value = kwargs.popitem()
+        return getattr(self, key)[value]
+
+    @staticmethod
+    def fabtabular():
+        import csv
+        import sys
+
+        if sys.version_info[0] == 2:
+            from urllib2 import urlopen
+            u = urlopen('http://www-01.sil.org/iso639%2D3/iso-639-3.tab')
+            return list(csv.reader(u, delimiter='\t'))[1:]
+        else:
+            from urllib.request import urlopen
+            import io
+            with io.StringIO(urlopen('http://www-01.sil.org/iso639%2D3/iso-639-3.tab').read().decode()) as u:
+                return list(csv.reader(u, delimiter='\t'))[1:]
+
+
 """ Use ISO 639-3 ?? """
 dash3 = True
 
@@ -67,34 +111,6 @@ assert(map_language('chi') is chinese)
 
 assert(map_language('ara') is arabic)
 assert(map_language('Arabic, Moroccan Spoken') is arabic)
-
-
-class language(object):
-    def __init__(self, dash3, dash2b, dash2t, dash1, name):
-        self.dash3 = dash3
-        self.dash2b = dash2b
-        self.dash2t = dash2t
-        self.dash1 = dash1
-        self.name = name
-
-
-class iso639_3(object):
-    def __init__(self):
-        self.languages = [language(a, b, c, d, g) for a, b, c, d, e, f, g, h in self._fabtabular_py3()]
-        self.dash3 = {x.dash3: x for x in self.languages}
-        self.dash2b = {x.dash2b: x for x in self.languages if x.dash2b}
-        self.dash2t = {x.dash2t: x for x in self.languages if x.dash3}
-        self.dash1 = {x.dash1: x for x in self.languages if x.dash1}
-
-    @staticmethod
-    def _fabtabular_py3():
-        import csv
-        from urllib.request import urlopen
-        import io
-
-        with io.StringIO(urlopen('http://www-01.sil.org/iso639%2D3/iso-639-3.tab').read().decode()) as u:
-            return list(csv.reader(u, delimiter='\t'))[1:]
-
 
 i = iso639_3()
 print(vars(i.languages[9]))
