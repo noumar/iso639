d325bc6dc5d07b55ffabe18fbeac5f48551effc3
Mikael Karlsson <i8myshoes@gmail.com>
2014-08-08 20:40:06 +0300

    Extract _fabtabular() out from iso_639_3()

--- iso_639_3/iso_639_3.py
+++ iso_639_3/iso_639_3.py
@@ -6,14 +6,42 @@ Licensed under AGPLv3.
 """
 
 
+def _fabtabular():
+    """
+    This function retrieves the ISO 639-3 and inverted names datasets as tsv files and returns them as two lists.
+    """
+    import csv
+    import sys
+    from pkg_resources import resource_filename
+
+    data = resource_filename(__package__, 'iso-639-3.tab')
+    inverted = resource_filename(__package__, 'iso-639-3_Name_Index.tab')
+
+    if sys.version_info[0] == 2:
+        # from urllib2 import urlopen
+        # u = urlopen('http://www-01.sil.org/iso639%2D3/iso-639-3.tab')
+        u = open(data)
+        # i = urlopen('http://www-01.sil.org/iso639-3/iso-639-3_Name_Index.tab')
+        i = open(inverted)
+        return list(csv.reader(u, delimiter='\t'))[1:], list(csv.reader(i, delimiter='\t'))[1:]
+    else:
+        # from urllib.request import urlopen
+        # import io
+        # with io.StringIO(urlopen('http://www-01.sil.org/iso639%2D3/iso-639-3.tab').read().decode()) as u, \
+        #         io.StringIO(urlopen('http://www-01.sil.org/iso639-3/iso-639-3_Name_Index.tab').read().decode()) as i:
+        with open(data) as u, \
+                open(inverted) as i:
+            return list(csv.reader(u, delimiter='\t'))[1:], list(csv.reader(i, delimiter='\t'))[1:]
+
+
 class iso_639_3(object):
     """
     This class is a close to drop-in replacement for pycountry.languages.
     But unlike pycountry.languages it also supports ISO 639-3.
     """
     def __init__(self):
-        l, i = self._fabtabular()
         self.languages = [self.language(a, b, c, d, e, [x[2] for x in i if x[0] == a][0])
+        l, i = _fabtabular()
                           for a, b, c, d, _, _, e, _ in l]
         self.alpha3 = {x.alpha3: x for x in self.languages if x.alpha3}
         self.bibliographic = {x.bibliographic: x for x in self.languages if x.bibliographic}
@@ -36,24 +64,3 @@ class iso_639_3(object):
             raise AttributeError("Only one keyword expected")
         key, value = kwargs.popitem()
         return getattr(self, key)[value]
-
-    @staticmethod
-    def _fabtabular():
-        import csv
-        import sys
-
-        if sys.version_info[0] == 2:
-            # from urllib2 import urlopen
-            # u = urlopen('http://www-01.sil.org/iso639%2D3/iso-639-3.tab')
-            u = open('iso-639-3.tab')
-            # i = urlopen('http://www-01.sil.org/iso639-3/iso-639-3_Name_Index.tab')
-            i = open('http://www-01.sil.org/iso639-3/iso-639-3_Name_Index.tab')
-            return list(csv.reader(u, delimiter='\t'))[1:], list(csv.reader(i, delimiter='\t'))[1:]
-        else:
-            # from urllib.request import urlopen
-            # import io
-            # with io.StringIO(urlopen('http://www-01.sil.org/iso639%2D3/iso-639-3.tab').read().decode()) as u, \
-            #         io.StringIO(urlopen('http://www-01.sil.org/iso639-3/iso-639-3_Name_Index.tab').read().decode()) as i:
-            with open('iso-639-3.tab') as u, \
-                    open('iso-639-3_Name_Index.tab') as i:
-                return list(csv.reader(u, delimiter='\t'))[1:], list(csv.reader(i, delimiter='\t'))[1:]
