76faa45c9b5883b85db5786e35e66bb2a4b01f30
Mikael Karlsson <i8myshoes@gmail.com>
2014-08-08 20:32:44 +0300

    Extract map_language() to logic.py

--- iso_639_3/iso_639_3.py
+++ iso_639_3/iso_639_3.py
@@ -5,30 +5,6 @@ Copyright (C) 2014 Mikael Karlsson.
 Licensed under AGPLv3.
 """
 
-import re
-
-
-def map_language(language):
-    if '_' in language:
-        language = language.split('_')[0]
-    if len(language) == 2:
-        try: return languages.get(alpha2=language.lower())
-        except KeyError: pass
-    elif len(language) == 3:
-        if dash3:
-            try: return languages.get(alpha3=language.lower())
-            except KeyError: pass
-        try: return languages.get(terminology=language.lower())
-        except KeyError: pass
-        try: return languages.get(bibliographic=language.lower())
-        except KeyError: pass
-    else:
-        try: return languages.get(name=language.title())
-        except KeyError: pass
-        for l in re.split('[,.;: ]+', language):
-            try: return languages.get(name=l.title())
-            except KeyError: pass
-
 
 class iso_639_3(object):
     """
---
+++ iso_639_3/logic.py
@@ -0,0 +1,31 @@
+import re
+
+""" Use ISO 639-3 ?? """
+dash3 = True
+
+if dash3:
+    languages = iso_639_3()
+else:
+    from pycountry import languages
+
+
+def map_language(language):
+    if '_' in language:
+        language = language.split('_')[0]
+    if len(language) == 2:
+        try: return languages.get(alpha2=language.lower())
+        except KeyError: pass
+    elif len(language) == 3:
+        if dash3:
+            try: return languages.get(alpha3=language.lower())
+            except KeyError: pass
+        try: return languages.get(terminology=language.lower())
+        except KeyError: pass
+        try: return languages.get(bibliographic=language.lower())
+        except KeyError: pass
+    else:
+        try: return languages.get(name=language.title())
+        except KeyError: pass
+        for l in re.split('[,.;: ]+', language):
+            try: return languages.get(name=l.title())
+            except KeyError: pass
