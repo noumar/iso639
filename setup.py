f6da92897008e5c03565c3b7fbbec2d2fb852b63
Mikael Karlsson <i8myshoes@gmail.com>
2015-02-12 11:11:29 +0200

    Bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iso639',
-    version='0.4.0',
+    version='0.4.1',
     description="Python library for ISO 639 standard",
     long_description=
     """
