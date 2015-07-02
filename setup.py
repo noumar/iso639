db88ad75057194aef4e2755ef643fdc5f292ae41
Mikael Karlsson <i8myshoes@gmail.com>
2015-07-02 16:02:49 +0300

    Set classifiers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 Python library for ISO 639 standard
 
-Copyright (c) 2014 Mikael Karlsson (CSC - IT Center for Science Ltd.).
+Copyright (c) 2014-2015 Mikael Karlsson (CSC - IT Center for Science Ltd.).
 Licensed under AGPLv3.
 """
 
@@ -14,7 +14,24 @@ setup(
     long_description=
     """
     """,
-    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Text Processing :: Linguistic',
+    ],
     keywords='pycountry language iso-639 iso 639 639-1 639-2 639-3 639-5',
     author='Mikael Karlsson',
     author_email='i8myshoes@gmail.com',
