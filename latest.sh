3a3a8685f74574e811928bc3bbc1200b64826b29
Mikael Karlsson <i8myshoes@gmail.com>
2015-07-07 12:09:22 +0300

    Add latest.sh

---
+++ latest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+wget -N "http://id.loc.gov/vocabulary/iso639-1.tsv"
+wget -N "http://id.loc.gov/vocabulary/iso639-2.tsv"
+wget -N "http://www-01.sil.org/iso639-3/iso-639-3.tab"
+wget -N "http://www-01.sil.org/iso639-3/iso-639-3_Name_Index.tab"
+wget -N "http://www-01.sil.org/iso639-3/iso-639-3-macrolanguages.tab"
+wget -N "http://www-01.sil.org/iso639-3/iso-639-3_Retirements.tab"
+wget -N "http://id.loc.gov/vocabulary/iso639-5.tsv"
